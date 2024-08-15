ddb1aaf0b36628888b4ba696dfc962c1c83ead7b
sridevi kandhalu <sridevikandhalu@sridevis-MacBook-Pro.local>
2024-08-14 20:44:31 -0500

    escape sequence characters

---
+++ 002-escCharacters.sh
@@ -0,0 +1,14 @@
+# /bin/bash
+
+#Escape Sequence characters
+
+# \n - new line
+# \t - tab space
+
+echo line1
+echo line2
+echo -e "line3\nline4\n"
+echo -e "Tab space between word1 \t word2"
+# Whenever you're using special characters , always enclose them in DOUBLE QUOTES
+# " "  : Double Quotes 
+# ' '  : Single Quotes  
