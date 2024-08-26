a9b40e7f9dcd88daf0954d5f812faa7b542c960e
sridevi kandhalu <sridevikandhalu@sridevis-MacBook-Pro.local>
2024-08-26 09:31:20 -0500

    Function scripting

--- 009-readvalue.sh
+++ 009-readvalue.sh
@@ -5,4 +5,4 @@ read -p "Enter Value Of A:" A
 read -p "Enter Value Of B:" B
 echo "Value of A is $A" 
 echo "Value of B is $B" 
-echo "Sum of A + B  = $((A+B))"   # Expresisons that are arithmetic should be enclosed in double paranthesis
+echo "Sum of A + B  = $((A+B))"   # Expressions that are arithmetic should be enclosed in double paranthesis
